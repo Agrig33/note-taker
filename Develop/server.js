55ccd41c330206e321c2ef77472d26fbd787abbf
{airin grigorian} <agrigorian33@gmail.com>
2024-03-14 13:57:10 -0700

    finished adding some changes

--- Develop/server.js
+++ Develop/server.js
@@ -3,15 +3,22 @@ const app = express();
 const fs = require("fs");
 const path = require("path");
 
-const api = require("./routes");
-const PORT = process.env.PORT || 3001
+const api = require("./routes/index");
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use("/api, api");
+app.use("/api", api);
 
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
 });
 
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT: ${PORT}`);
+});
