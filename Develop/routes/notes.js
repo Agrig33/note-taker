decca17bb973286bb5fe0e130e1d61721bf497cd
{airin grigorian} <agrigorian33@gmail.com>
2024-03-14 15:29:08 -0700

    added get route for data on notes.js

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,4 +1,14 @@
+//Global Variables
+
 const router = require("express").Router();
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid")
+
+//Get route for data
+router.get("/", (req, res) => {
+    fs.readFile("./Develop/db/db.json", (err,data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
+});
 
